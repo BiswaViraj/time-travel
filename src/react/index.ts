e5c5744780d0df641c63abc6178a7f3eab2fb80a
Biswajeet Das <biswajeetdas18@gmail.com>
2026-03-29 12:08:06 +0530

    fix: use useMemo for hook return, remove unused jest-dom dep

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,4 @@
-import { useRef, useSyncExternalStore } from "react";
+import { useMemo, useRef, useSyncExternalStore } from "react";
 import { timeTravel } from "../index";
 import type { TimeTravel, TimeTravelOptions } from "../types";
 
@@ -13,21 +13,13 @@ export function useTimeTravel<T>(
 	initialValue: T,
 	options?: TimeTravelOptions,
 ): UseTimeTravel<T> {
-	type Mutable = TimeTravel<T> & { state: T };
-
-	const resultRef = useRef<Mutable>(undefined);
-	if (!resultRef.current) {
-		const tt = timeTravel(initialValue, options);
-		resultRef.current = Object.assign(Object.create(tt), {
-			state: tt.get(),
-		});
+	const ref = useRef<TimeTravel<T> | null>(null);
+	if (!ref.current) {
+		ref.current = timeTravel(initialValue, options);
 	}
+	const tt = ref.current;
 
-	resultRef.current.state = useSyncExternalStore(
-		resultRef.current.subscribe,
-		resultRef.current.get,
-		resultRef.current.get,
-	);
+	const state = useSyncExternalStore(tt.subscribe, tt.get, tt.get);
 
-	return resultRef.current;
+	return useMemo(() => ({ ...tt, state }), [tt, state]);
 }
